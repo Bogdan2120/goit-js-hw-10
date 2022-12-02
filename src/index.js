dc5cf0a604e6d14792ac68c73c9788774fccd675
Bogdan Vasylyk <bogdanvasilik2120@gmail.com>
2022-12-02 22:59:50 +0200

    created by fetchCountries

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+import { fetchCountries } from './fetchCountries';
+
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+fetchCountries('USA')
+  .then(data => {
+    console.log(data);
+    // Data handling
+  })
+  .catch(error => {
+    // Error handling
+  });
