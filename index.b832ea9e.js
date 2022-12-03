1ce625e0453c16369666827fd1b1966becd51f8c
Bogdan2120 <bogdanvasilik2120@gmail.com>
2022-12-03 20:38:00 +0000

    Deploying to gh-pages from @ Bogdan2120/goit-js-hw-10@3429c16a98edb81d2a4109def171f9acc5e8420c 🚀

--- index.b832ea9e.js
+++
@@ -1,2 +0,0 @@
-!function(){var n;(n="USA",fetch("https://restcountries.com/v3.1/name/".concat(n)).then((function(n){if(!n.ok)throw new Error(n.status);return n.json()}))).then((function(n){console.log(n)})).catch((function(n){}))}();
-//# sourceMappingURL=index.b832ea9e.js.map
